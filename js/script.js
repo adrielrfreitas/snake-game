e0d695157b8f4f888c68e68d3c7a415e67017976
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-08-09 11:19:01 -0300

    active function snake move

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const ctx = canvas.getContext("2d")
 const size = 20
 
 const snake = [ 
-    { x: 200, y: 200},
-    { x: 220, y: 200}
+    { x: 0, y: 0},
+    { x: 20, y: 0}
 ]
 
-let direction 
+let direction, loopId
 
 const drawSnake = () => 
 {
@@ -55,11 +55,12 @@ const moveSnake = () =>
 
 const gameLoop = () => 
 {
+    clearInterval(loopId)
     ctx.clearRect(0, 0, 600, 600)
     moveSnake()
     drawSnake()
 
-    setTimeout(() => 
+    loopId = setTimeout(() => 
         {
             gameLoop()
         }, 300)
@@ -67,5 +68,46 @@ const gameLoop = () =>
 
 gameLoop()
 
+document.addEventListener("keydown", ({key}) => 
+{
+    if (key == "ArrowRight" && direction != "left" && direction != "a")
+    {
+        direction = "right"
+    }
 
+    if (key == "ArrowLeft" && direction != "right" && direction != "d")
+    {
+        direction = "left"
+    }
+
+    if (key == "ArrowUp" && direction != "down" && direction != "s")
+    {
+        direction = "up"
+    }
+
+    if (key == "ArrowDown" && direction != "up" && direction != "w")
+    {
+        direction = "down"
+    } 
+        
+    if (key == "d" && direction != "left" && direction != "a")
+    {
+        direction = "right"
+    }
+        
+    if (key == "a" && direction != "right" && direction != "d")
+    {
+        direction = "left"
+    }
+        
+    if (key == "w" && direction != "down" && direction != "s")
+    {
+        direction = "up"
+    }
+        
+    if (key == "s" && direction != "up" && direction != "w")
+    {
+        direction = "down"
+    }    
+})
 
